bd016643415abf569787e86cd9ddb4fcf0553f48
manel turki <menelturki@gmail.com>
2020-02-13 14:09:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
   if (left > 0) {
-    dodger.style.left = `${left + 1}px`
+    dodger.style.left = left+1
   }
 }
